9e50c499795c045fc8b7457be2d5fdf8bbfa033b
Arve Knudsen <arve.knudsen@gmail.com>
2016-08-27 19:34:36 +0200

    Use imagesloaded to detect that child elements are ready

--- src/react-infinite-scroll.js
+++ src/react-infinite-scroll.js
@@ -1,3 +1,7 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+let imagesLoaded = require('imagesloaded')
+
 function topPosition(domElt) {
   if (!domElt) {
     return 0;
@@ -5,7 +9,7 @@ function topPosition(domElt) {
   return domElt.offsetTop + topPosition(domElt.offsetParent);
 }
 
-module.exports = function (React) {
+module.exports = function () {
   if (React.addons && React.addons.InfiniteScroll) {
     return React.addons.InfiniteScroll;
   }
@@ -22,8 +26,10 @@ module.exports = function (React) {
     },
     componentDidMount: function () {
       this.attachScrollListener();
+      this.scrollListener();
     },
     componentDidUpdate: function () {
+      console.log(`Reattaching scroll listener because component updated itself`)
       this.attachScrollListener();
     },
     render: function () {
@@ -31,26 +37,45 @@ module.exports = function (React) {
       return React.DOM.div(null, props.children, props.hasMore && props.loader);
     },
     scrollListener: function () {
-      var el = this.getDOMNode();
-      var scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
-      if (topPosition(el) + el.offsetHeight - scrollTop - window.innerHeight < Number(this.props.threshold)) {
+      console.log(`Handling scroll event`, this)
+      var el = ReactDOM.findDOMNode(this);
+      var scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset :
+        (document.documentElement || document.body.parentNode || document.body).scrollTop;
+      var delta = topPosition(el) + el.offsetHeight - scrollTop - window.innerHeight;
+      if (delta < Number(this.props.threshold)) {
+        console.log(`event, Scroll delta is less than threshold: ${delta}, loading more`)
         this.detachScrollListener();
         // call loadMore after detachScrollListener to allow
         // for non-async loadMore functions
-        this.props.loadMore(this.props.pageStart += 1);
+        this.props.loadMore(this.props.pageStart);
+        this.props.pageStart += 1;
+      } else {
+        console.log(`event, Scroll delta is larger than or equal to threshold: ${delta}`)
       }
     },
     attachScrollListener: function () {
+      console.log(`Attaching scroll event listener`)
       if (!this.props.hasMore) {
         return;
       }
-      window.addEventListener('scroll', this.scrollListener);
-      window.addEventListener('resize', this.scrollListener);
-      this.scrollListener();
+
+      var imageLoad = imagesLoaded(ReactDOM.findDOMNode(this))
+
+      function handleImagesLoaded() {
+        console.log('All images are loaded, listening to events')
+        global.addEventListener('scroll', this.scrollListener)
+        global.addEventListener('resize', this.scrollListener)
+        this.scrollListener();
+
+        imageLoad.off('always', handleImagesLoaded)
+      }
+
+      imageLoad.on('always', handleImagesLoaded.bind(this))
     },
     detachScrollListener: function () {
-      window.removeEventListener('scroll', this.scrollListener);
-      window.removeEventListener('resize', this.scrollListener);
+      console.log('event detaching scroll listener')
+      global.removeEventListener('scroll', this.scrollListener);
+      global.removeEventListener('resize', this.scrollListener);
     },
     componentWillUnmount: function () {
       this.detachScrollListener();
